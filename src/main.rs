579e511a7203268e2e463b67abda9d15af8316dc
Jared Simpson <jared.simpson@gmail.com>
2023-05-29 15:13:11 -0400

    hack together program that matches list of barcodes to reads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{Parser, Subcommand};
+use bio::io::fasta;
 use bio::io::fastq;
-use bio::pattern_matching::myers::long::Myers;
+use bio::pattern_matching::myers::Myers;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -19,6 +20,7 @@ enum Commands {
         #[arg(short, long, value_name = "prefix")]
         suffix: String,
         */
+        barcode: String,
         fastq: String
         },
 }
@@ -29,123 +31,68 @@ fn main() {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::Split { fastq } => {
-            split_reads(fastq);
+        Commands::Split { barcode, fastq } => {
+            split_reads(barcode, fastq);
         }
     }
 }
 
-fn filter_overlaps(occ: Vec<(usize, usize, usize)>) -> Vec<(usize, usize, usize)> {
+fn get_best_match(occ: Vec<(usize, usize, u8)>) -> Option<(usize, usize, u8)> {
 
-    let mut out = Vec::<(usize, usize, usize)>::new();
-    // lazy naive method n^2, discard any hits that overlap a hit with a lower edit distance
+    let mut out: Option<(usize, usize, u8)> = None;
+
     for a in occ {
-
-        let mut add = true;
-        for b in out.iter_mut() {
-            // check overlap
-            let overlaps = a.0 <= b.1 && b.0 <= a.1;
-            if overlaps {
-                if a.2 <= b.2 { 
-                    // better hit, replace b
-                    *b = a;
-                }
-
-                add = false; 
-                break;
-            }
+        if out.is_none() || a.2 < out.unwrap().2 {
+            out = Some(a);
         }
-
-        if add { out.push(a); } // no overlaps, add to collection
     }
 
     return out;
 }
 
-/*
-fn display_matches(patterns: [(Myers<u128>, Myers<u128>); 2], seq: &[u8]) -> () {
+fn split_reads(input_barcode: &str, input_fastq: &str) {
 
-    let mut match_strings = Vec::new();
-    for si in 0..=1 {
-        let mut strand_matches = vec![' '; seq.len()];
-
-        let occ_prefix: Vec<_> = filter_overlaps(strand_pattern[si].0.find_all(seq, max_ed).collect());
-        let occ_suffix: Vec<_> = filter_overlaps(strand_pattern[si].1.find_all(seq, max_ed).collect());
-
-        for hit in &occ_prefix {
-
-            let mut e = hit.0 + strand_seq[si][0].len(); 
-            e = if e > strand_matches.len() { strand_matches.len() } else { e };
-            strand_matches.splice(hit.0 .. e, strand_seq[si][0].chars());
+    let max_ed = 4;
+    let barcode_reader = fasta::Reader::from_file(input_barcode).expect("Could not open input fasta");
+    let mut patterns = Vec::new();
+    for r in barcode_reader.records() {
+        if let Ok(record) = r {
+            patterns.push( (record.id().to_owned(), Myers::<u64>::new(record.seq()) ));
         }
-
-        for hit in &occ_suffix {
-            let mut e = hit.0 + strand_seq[si][1].len(); 
-            e = if e > strand_matches.len() { strand_matches.len() } else { e };
-            strand_matches.splice(hit.0 .. e, strand_seq[si][1].chars());
-        }
-
-        let sm: String = strand_matches.into_iter().collect();
-        println!("POC: {:?}", occ_prefix);
-        println!("SOC: {:?}", occ_suffix);
-        match_strings.push(sm);
     }
 
-    let pp_seq = String::from_utf8_lossy(seq);
-    let l = pp_seq.len();
-    let step = 80;
-    for i in (0..l).step_by(step) {
-        let m = if i + step >= l { l - 1 } else { i + step };
-        println!("S1: {}", &match_strings[0][i..m]);
-        println!("FQ: {}", &pp_seq[i..m]);
-        println!("S2: {}", &match_strings[1][i..m]);
-        println!("");
-    }
-
-}
-*/
-
-fn split_reads(input_fastq: &str) {
     let reader = fastq::Reader::from_file(input_fastq).expect("Could not open input fastq");
-    let max_ed = 14;
-
-    let strand_seq = [ [ "GAATCCTCGGATTCCATGATCGTTACATGATTTTCTGTTGGTGCTGATATTGC", 
-                         "CTTGCGGGCGGCGGACTCTCCTCTGAAGATAGAGCGACAGGCAAGTCACAAAGACACCGACAACTTTCTTGTCAC" ],
-                       [ "GTGACAAGAAAGTTGTCGGTGTCTTTGTGACTTGCCTGTCGCTCTATCTTCAGAGGAGAGTCCGCCGCCCGCAAG", 
-                         "GCAATATCAGCACCAACAGAAAATCATGTAACGATCATGGAATCCGAGGATTC" ] ];
-
-    let mut strand_pattern = [ (Myers::<u64>::new(strand_seq[0][0].bytes()), Myers::<u64>::new(strand_seq[0][1].bytes())),
-                               (Myers::<u64>::new(strand_seq[1][0].bytes()), Myers::<u64>::new(strand_seq[1][1].bytes())) ];
     
     for r in reader.records() {
         if let Ok(record) = r {
+
             let seq = record.seq();
+            let mut hits = Vec::new();
+            
+            // find lowest distance hit between each barcode and read sequence
+            for p in patterns.iter_mut() {
+                hits.push( get_best_match(p.1.find_all(seq, max_ed).collect()) );
+            }
 
-            let mut subs = Vec::new();
-
-            for si in 0..=1 {
-
-                let occ_prefix: Vec<_> = filter_overlaps(strand_pattern[si].0.find_all(seq, max_ed).collect());
-                let occ_suffix: Vec<_> = filter_overlaps(strand_pattern[si].1.find_all(seq, max_ed).collect());
-
-                for idx in 0..occ_prefix.len() {
-                    let start = occ_prefix[idx];
-                    let end = if idx < occ_suffix.len() { Some(occ_suffix[idx]) } else { None };
-                    subs.push( (start, end, si) );
+            // parse vector of hits to find best (lowest edit distance)
+            let mut best_barcode: Option<usize> = None;
+            let mut best_hit: Option<(usize, usize, u8)>= None;
+            for (idx, h) in hits.iter().enumerate() {
+                if h.is_some() {
+                    let t = h.unwrap();
+                    let ed = t.2;
+                    if best_hit.is_none() || ed < best_hit.unwrap().2 {
+                        best_barcode = Some(idx);
+                        best_hit = Some(t);
+                    }
                 }
             }
 
-            subs.sort_by(|a, b| a.0.0.cmp( & b.0.0 ));
-            let mut count = 0;
-            for (start, end, strand) in subs {
-
-                let s = start.1 + 1; // endpoint of front match
-                let e = if let Some(t) = end { t.0 } else { seq.len() };
-                if e < s { continue; }               
-                println!(">{}_{} strand={} start={:?} end={:?}", record.id(), count, strand, start, end);
-                count += 1;
-                let insert = &seq[s..e];
-                println!("{}", std::str::from_utf8(&insert).unwrap());
+            if best_hit.is_some() {
+                let b = best_hit.unwrap();
+                println!("{}\t{}\t{}\t{}\t{}", record.id(), patterns[best_barcode.unwrap()].0, b.2, b.0, b.1); 
+            } else {
+                println!("{}\t-\t-\t-\t-", record.id());
             }
         }
     }
